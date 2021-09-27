c218629c899b6ba198181d786edfe07f3bcf0069
LULU ALFINA ZAHRA <91053950+Lulualfinaa@users.noreply.github.com>
2021-09-27 14:39:34 +0700

    Upload Praktikum 2 tugas 1

---
+++ Praktikum 2/LuluAlfinaZahra-064002100004-praktikum 2-tugas 1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 12:35:33 2021
+
+@author: lulualfina15
+"""
+
+import math
+
+a = float(input("masukkan nilai a : "))
+b = float(input("msukkan nilai b : "))
+
+
+hasil = a + b
+print("a ditambah b :", hasil)
+
+hasil = a-b
+print("selisih a dan b :", abs(hasil))
+
+hasil = a * b
+print("a dikali b :", hasil)
+
+hasil = a % b
+print("sisa pembagian a dan b :", hasil)
+
+hasil = a/b
+print("pembagian a dan b :", hasil)
+
+hasil = math.log10(a)
+print("log (a) :", hasil)
+
+hasil = a ** b
+print("pangkat a dan b :", hasil)
+
+
+
+
+
