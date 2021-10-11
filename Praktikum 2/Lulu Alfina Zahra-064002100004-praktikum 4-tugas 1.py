e9a22a499704888670fa98a710042a168d72dd30
LULU ALFINA ZAHRA <91053950+Lulualfinaa@users.noreply.github.com>
2021-10-11 13:48:27 +0700

    praktikum 4 tugas 1

---
+++ Praktikum 2/Lulu Alfina Zahra-064002100004-praktikum 4-tugas 1.py
@@ -0,0 +1,11 @@
+Created on Mon Oct 11 13:14:47 2021
+
+@author: lulualfina15
+"""
+
+y= int(input("masukkan angka: "))
+while y>0:
+     for i in range (y):
+         print(y,end="")
+     print()
+     y -= 1   
